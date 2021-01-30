1b5dfb12f052b2dc0fe1c9a9891e9107195b3c74
hongmingzhang <hzhangal0330@gmail.com>
2021-01-30 16:04:00 -0500

    Hongming_test

--- Hongming_modify_ace.py
+++ Hongming_modify_ace.py
@@ -7,7 +7,7 @@ folder_name = '/shared/lyuqing/probing_for_event/data/ACE_oneie/en/event_only'
 tmp_data = list()
 with open(folder_name+'/'+'dev.event.json', 'r') as f:
     for line in f:
-        tmp_example = json.loads(f)
+        tmp_example = json.loads(line)
         tmp_data.append(tmp_example)
     # tmp_data = json.load(f)
 
