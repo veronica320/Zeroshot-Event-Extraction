697799172f548eb10b921a54fb1cb7979e24c9bc
hongmingzhang <hzhangal0330@gmail.com>
2021-01-30 16:55:47 -0500

    Hongming_test

--- Hongming_modify_ace.py
+++ Hongming_modify_ace.py
@@ -22,37 +22,41 @@ def generate_new_data(file_name):
 
     final_result = list()
     print('Number of docs:', len(sentence_by_doc))
-    for tmp_doc in sentence_by_doc:
+    for tmp_doc in tqdm(sentence_by_doc):
         print(tmp_doc)
         print('Number of sentences:', len(sentence_by_doc[tmp_doc]))
-        for tmp_example in tqdm(sentence_by_doc[tmp_doc]):
+        for tmp_example in sentence_by_doc[tmp_doc]:
             old_sentence = tmp_example['sentence']
-            # old_tokens = tmp_example['tokens']
-            event_types = list()
+            old_tokens = tmp_example['tokens']
+            local_event_types = list()
             for tmp_e in tmp_example['event_mentions']:
-                event_types.append(tmp_e['event_type'])
-            event_types = set(event_types)
-            all_sentences = list()
-            # all_tokens = list()
-            print(event_types)
+                local_event_types.append(tmp_e['event_type'])
+            local_event_types = set(local_event_types)
+            local_sentences = list()
+            all_tokens = list()
+            print(local_event_types)
             for tmp_example_2 in sentence_by_doc[tmp_doc]:
                 same_event_types = False
-                # for tmp_event_2 in tmp_example_2['event_mentions']:
-                #     if tmp_event_2['event_type'] in event_types:
-                #         same_event_types = True
-                #         break
-                # if same_event_types:
-                #     continue
-                all_sentences.append(tmp_example_2['sentence'])
+                for tmp_event_2 in tmp_example_2['event_mentions']:
+                    if tmp_event_2['event_type'] in local_event_types:
+                        same_event_types = True
+                        break
+                if same_event_types:
+                    continue
+                local_sentences.append(tmp_example_2['sentence'])
                 # print(all_sentences)
-                # all_tokens += tmp_example_2['tokens']
-            all_sentences.append(old_sentence)
+                all_tokens += tmp_example_2['tokens']
+            local_sentences.append(old_sentence)
             # all_tokens.append(old_tokens)
-            tmp_example['original_sentence'] = old_sentence
-            # tmp_example['original_tokens'] = old_tokens
-            tmp_example['sentence'] = ' '.join(all_sentences)
-            # tmp_example['tokens'] = all_tokens
-            final_result.append(tmp_example)
+            new_example = dict()
+            for tmp_key in tmp_example:
+                new_example[tmp_key] = tmp_example[tmp_key]
+
+            new_example['original_sentence'] = old_sentence
+            new_example['original_tokens'] = old_tokens
+            new_example['sentence'] = ' '.join(local_sentences)
+            new_example['tokens'] = all_tokens
+            final_result.append(new_example)
     random.shuffle(final_result)
     with open(folder_name + '/modified.' + file_name, 'r') as f:
         for tmp_example in final_result:
